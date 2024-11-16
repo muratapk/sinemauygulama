1cd28c092d5e08998ed99ba9eec9f1024ac8cd17
ARIBİLGİ <ARIBLG@PC105E>
2024-11-16 16:22:44 +0300

    Add project files.

---
+++ sinemauygulama/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sinemauygulama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = false;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+            button5.Enabled = true;
+        }
+    }
+}
---
+++ sinemauygulama/sinema.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sinemauygulama
+{
+    public class sinema
+    {
+       private double ogrenci;
+       private double tam;
+       private double bakiye;
+       private int boskoltuksayisi;
+       private int koltuksayisi;
+       private string salonno;
+
+        public sinema(string salonno,int koltuksayisi,double ogrenci,double tam)
+        {
+            this.ogrenci = ogrenci;
+            this.tam = tam;
+            this.boskoltuksayisi=koltuksayisi;
+            this.salonno = salonno;
+            this.bakiye = 0;
+        }
+        public void Biletsat(bool indirimli)
+        {
+            boskoltuksayisi--;
+            if(indirimli)
+            {
+                bakiye += ogrenci;
+            }
+            else
+            {
+                bakiye += tam;
+            }
+           
+        }
+        public void Biletiptal(bool indirimli)
+        {
+            boskoltuksayisi++;//bir artır
+            if(indirimli)
+            {
+                bakiye -= ogrenci;
+            }
+            else
+            {
+                bakiye -= tam;
+            }
+
+        }
+        public string BakiyeOgren()
+        {
+            return bakiye.ToString();
+        }
+        public string BosKoltukOgren()
+        {
+            return boskoltuksayisi.ToString();
+        }
+    }
+}
